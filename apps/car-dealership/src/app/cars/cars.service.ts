72c73847bee04e42aacb685b3609f1c60853761a
alexelizaga <alexelizaga@icloud.com>
2023-09-14 18:37:11 +0200

    Crear un nuevo coche

--- apps/car-dealership/src/app/cars/cars.service.ts
+++ apps/car-dealership/src/app/cars/cars.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
+
 import { Car } from './interfaces/car.interface';
+import { CreateCarDto } from './dtos/create-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -30,5 +36,19 @@ export class CarsService {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
     return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const exist = this.cars.find((car) => car.model === createCarDto.model);
+    if (exist)
+      throw new BadRequestException(
+        `Car with model '${createCarDto.model}' already exist`
+      );
+    const newCar: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+    this.cars.push(newCar);
+    return newCar;
   }
 }
