c99627e1dcd6fd6c7259590a3c13667833f189ee
alexelizaga <alexelizaga@icloud.com>
2023-09-17 08:00:17 +0200

    79

--- apps/pokedex/src/app.module.ts
+++ apps/pokedex/src/app.module.ts
@@ -9,7 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '.', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
     PokemonModule,
   ],
   controllers: [],
